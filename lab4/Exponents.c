cc093ff05db5bb51969e1b9eb47d4da74e2b3256
unknown <guitethangkhanmuan@gmail.com>
2024-02-28 11:56:09 +0530

    Changed variable names

--- lab4/Exponents.c
+++ lab4/Exponents.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int num, pow, result = 1;
+    int base, pow, result = 1;
 
     printf("Enter a number: ");
-    scanf("%d", &num);
+    scanf("%d", &base);
 
     printf("Enter power: ");
     scanf("%d", &pow);
 
     for (int i = 0; i < pow; ++i){
-        result *= num;
+        result *= base;
     }
     printf("Result: %d\n", result);
 
